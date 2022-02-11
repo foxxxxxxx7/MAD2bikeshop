2a46e89b1bfb32f7c0dfdb62effadb9ef4b8b7d9
Rob Fox <60361504+foxxxxxxx7@users.noreply.github.com>
2022-02-11 22:58:00 +0000

    Create Home activity

---
+++ app/src/main/java/com/wit/mad2bikeshop/activities/Home.kt
@@ -0,0 +1,12 @@
+package com.wit.mad2bikeshop.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wit.mad2bikeshop.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
