b6accc16b26a2a1c045b3003317a391e5c60be07
Rob Fox <60361504+foxxxxxxx7@users.noreply.github.com>
2022-02-12 15:13:56 +0000

    Update Home.kt
    Allow for NavDrawer, by linking navController, DrawerLayout and HomeBinding

--- app/src/main/java/com/wit/mad2bikeshop/activities/Home.kt
+++ app/src/main/java/com/wit/mad2bikeshop/activities/Home.kt
@@ -2,11 +2,27 @@ package com.wit.mad2bikeshop.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.wit.mad2bikeshop.R
+import com.wit.mad2bikeshop.databinding.HomeBinding
+
+private lateinit var drawerLayout: DrawerLayout
+private lateinit var homeBinding : HomeBinding
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.content_home)
+        homeBinding = HomeBinding.inflate(layoutInflater)
+        setContentView(homeBinding.root)
+        drawerLayout = homeBinding.drawerLayout
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
