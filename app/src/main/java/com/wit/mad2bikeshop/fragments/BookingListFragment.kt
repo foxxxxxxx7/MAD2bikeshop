758f9a6e2c6debfde6774d5f4b0841d3d0641ba6
Rob Fox <60361504+foxxxxxxx7@users.noreply.github.com>
2022-03-31 13:26:18 +0100

    Update BookingListFragment.kt
    Notify user when deleting booking

--- app/src/main/java/com/wit/mad2bikeshop/fragments/BookingListFragment.kt
+++ app/src/main/java/com/wit/mad2bikeshop/fragments/BookingListFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -64,6 +65,7 @@ class BookingListFragment : Fragment(), BookListener {
     override fun onDeleteBooking(booking: BookModel) {
         app.bookStore.delete(booking)
         showBookings(app.bookStore.findAll())
+        Toast.makeText(context, "Booking Deleted!", Toast.LENGTH_LONG).show()
 
     }
 
