ff0bebf101f3102d49f1a2287b88ae3e9d16ae6d
Rob Fox <60361504+foxxxxxxx7@users.noreply.github.com>
2022-04-07 15:23:47 +0100

    Add BookingDetailFrag + ViewModel

---
+++ app/src/main/java/com/wit/mad2bikeshop/BookingDetailFragment.kt
@@ -0,0 +1,31 @@
+package com.wit.mad2bikeshop
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class BookingDetailFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = BookingDetailFragment()
+    }
+
+    private lateinit var viewModel: BookingDetailViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.booking_detail_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(BookingDetailViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/wit/mad2bikeshop/BookingDetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.wit.mad2bikeshop
+
+import androidx.lifecycle.ViewModel
+
+class BookingDetailViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
