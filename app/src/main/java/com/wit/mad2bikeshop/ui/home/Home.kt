a7ccaec0823e16956feb5cabdd86fa06cdc08729
Rob Fox <60361504+foxxxxxxx7@users.noreply.github.com>
2022-04-30 18:27:12 +0100

    Update Home.kt

--- app/src/main/java/com/wit/mad2bikeshop/ui/home/Home.kt
+++ app/src/main/java/com/wit/mad2bikeshop/ui/home/Home.kt
@@ -1,39 +1,78 @@
 package com.wit.mad2bikeshop.ui.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.*
+import com.google.firebase.auth.FirebaseUser
 import com.wit.mad2bikeshop.R
 import com.wit.mad2bikeshop.databinding.HomeBinding
-
-private lateinit var drawerLayout: DrawerLayout
-private lateinit var homeBinding: HomeBinding
-
+import com.wit.mad2bikeshop.databinding.NavHeaderBinding
+import com.wit.mad2bikeshop.ui.auth.LoggedInViewModel
+import com.wit.mad2bikeshop.ui.auth.Login
 
 class Home : AppCompatActivity() {
 
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var homeBinding : HomeBinding
+    private lateinit var navHeaderBinding : NavHeaderBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var loggedInViewModel : LoggedInViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         homeBinding = HomeBinding.inflate(layoutInflater)
         setContentView(homeBinding.root)
         drawerLayout = homeBinding.drawerLayout
-
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        val navHostFragment = supportFragmentManager.
+        findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+
+        appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.bookFragment, R.id.bookingListFragment), drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
         val navView = homeBinding.navView
         navView.setupWithNavController(navController)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return NavigationUI.navigateUp(navController, drawerLayout)
+    public override fun onStart() {
+        super.onStart()
+        loggedInViewModel = ViewModelProvider(this).get(LoggedInViewModel::class.java)
+        loggedInViewModel.liveFirebaseUser.observe(this, Observer { firebaseUser ->
+            if (firebaseUser != null)
+                updateNavHeader(loggedInViewModel.liveFirebaseUser.value!!)
+        })
+
+        loggedInViewModel.loggedOut.observe(this, Observer { loggedout ->
+            if (loggedout) {
+                startActivity(Intent(this, Login::class.java))
+            }
+        })
+
     }
 
+    private fun updateNavHeader(currentUser: FirebaseUser) {
+        var headerView = homeBinding.navView.getHeaderView(0)
+        navHeaderBinding = NavHeaderBinding.bind(headerView)
+        navHeaderBinding.navHeaderEmail.text = currentUser.email
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
 }
