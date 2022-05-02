160d155ae0e42792ceb4010f64689fc077d820fd
Rob Fox <60361504+foxxxxxxx7@users.noreply.github.com>
2022-05-02 14:49:00 +0100

    Update BookStore
    New google-service + firebase sdk

--- app/src/main/java/com/wit/mad2bikeshop/model/BookStore.kt
+++ app/src/main/java/com/wit/mad2bikeshop/model/BookStore.kt
@@ -1,9 +1,18 @@
 package com.wit.mad2bikeshop.model
 
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.auth.FirebaseUser
+
 interface BookStore {
-    fun findAll() : List<BookModel>
-    fun findById(id: Long) : BookModel?
-    fun create(booking: BookModel)
-    fun update(booking: BookModel)
-    fun delete(booking: BookModel)
-}
+        fun findAll(bookingsList:
+                    MutableLiveData<List<BookModel>>
+        )
+        fun findAll(userid:String,
+                    bookingsList:
+                    MutableLiveData<List<BookModel>>)
+        fun findById(userid:String, bookingid: String,
+                     booking: MutableLiveData<BookModel>)
+        fun create(firebaseUser: MutableLiveData<FirebaseUser>, booking: BookModel)
+        fun delete(userid:String, bookingid: String)
+        fun update(userid:String, bookingid: String, booking: BookModel)
+    }
