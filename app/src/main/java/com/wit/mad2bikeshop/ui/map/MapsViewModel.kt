fc6a89460cc24ba162c7c402daa4b9e41e957326
Rob Fox <60361504+foxxxxxxx7@users.noreply.github.com>
2022-05-07 19:52:35 +0100

    Update MapsViewModel.kt

--- app/src/main/java/com/wit/mad2bikeshop/ui/map/MapsViewModel.kt
+++ app/src/main/java/com/wit/mad2bikeshop/ui/map/MapsViewModel.kt
@@ -1,13 +1,33 @@
 package com.wit.mad2bikeshop.ui.map
 
+import android.annotation.SuppressLint
+import android.app.Application
 import android.location.Location
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.GoogleMap
+import timber.log.Timber
 
-class MapsViewModel : ViewModel() {
+@SuppressLint("MissingPermission")
+class MapsViewModel(application: Application) : AndroidViewModel(application) {
 
     lateinit var map : GoogleMap
     var currentLocation = MutableLiveData<Location>()
+    var locationClient : FusedLocationProviderClient
 
+    init {
+        locationClient = LocationServices.getFusedLocationProviderClient(application)
+    }
+
+    fun updateCurrentLocation() {
+        if(locationClient.lastLocation.isSuccessful)
+            locationClient.lastLocation
+                .addOnSuccessListener { location: Location? ->
+                    currentLocation.value = location!!
+                }
+        Timber.i("LOC : %s", currentLocation.value)
+    }
 }
