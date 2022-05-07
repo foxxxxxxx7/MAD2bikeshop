aecbc5c376d170693f3192e766aca12e402971d3
Rob Fox <60361504+foxxxxxxx7@users.noreply.github.com>
2022-05-07 12:35:24 +0100

    Update FirebaseImageManager

--- app/src/main/java/com/wit/mad2bikeshop/firebase/FirebaseImageManager.kt
+++ app/src/main/java/com/wit/mad2bikeshop/firebase/FirebaseImageManager.kt
@@ -49,6 +49,7 @@ object FirebaseImageManager {
                 uploadTask.addOnSuccessListener { ut ->
                     ut.metadata!!.reference!!.downloadUrl.addOnCompleteListener { task ->
                         imageUri.value = task.result!!
+                        FirebaseDBManager.updateImageRef(userid,imageUri.value.toString())
                     }
                 }
             }
